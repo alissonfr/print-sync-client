5a479c191a47f51ff1d34c1b48b5ec168792fe67
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2023-09-27 22:37:58 -0300

    refactor: send image

--- app/main.py
+++ app/main.py
@@ -1,8 +1,42 @@
+from socket_client import SocketClient
 import webview
+import keyboard
+import pyautogui
+import base64
+import tempfile
+from io import BytesIO
 
-webview.create_window(title="Print-sync", url="template/index.html", width = 600, height = 800, resizable=False)
-webview.start()
+class Api:
+    def __init__(self):
+        self._window = None
+        self.client = None
 
-class InputValueReceiver:
-    def send_value(self, value): 
-        webview.handle_input_value(value)
+    def set_window(self, window):
+        self._window = window
+
+    def init_connection(self, username):
+        self.client = SocketClient('ws://25.62.141.82:5000', username)
+
+    def capture_encode(self):
+        photo = pyautogui.screenshot()
+        output = BytesIO()
+        photo.save(output, format='PNG')
+        image_data = base64.b64encode(output.getvalue()).decode()
+        return image_data
+
+    def handle_capture(self):
+        base64_string = self.capture_encode()
+        self.client.capture_message(base64_string)
+
+if __name__ == '__main__':
+    api = Api()
+    window = webview.create_window(title="Print-sync", url="template/index.html", width=600, height=800, resizable=False, js_api=api)
+    api.set_window(window)
+    webview.start()
+    while True:
+        if not keyboard.is_pressed('f'):
+            is_pressed = False
+        while not is_pressed:
+            if keyboard.is_pressed('f'):
+                api.handle_capture()
+                is_pressed = True
---
+++ app/socket_client.py
@@ -0,0 +1,42 @@
+import socketio
+import json
+from datetime import datetime
+
+class SocketClient:
+    def __init__(self, server_url, user):
+        self.sio = socketio.Client()
+        self.username = user
+        self.sio.connect(server_url)
+
+        self.sio.on('connect', self.connect)
+        self.sio.on('response', self.response)
+        self.sio.on('capture', self.capture)
+        self.sio.on('connect_error', self.connect_error)
+        self.sio.on('disconnect', self.disconnect)
+
+    def connect(self):
+        print('Conectado ao servidor')
+
+    def connect_error(self, data):
+        print("Falha na comunicação com o servidor! ", data)
+
+    def capture(self, data):
+        print('Mensagem do servidor no tópico "capture":', data)
+
+    def response(self, data):
+        print('Mensagem do tópico "response":', data)
+
+    def disconnect(self):
+        print('Desconectado do servidor')
+
+    def capture_message(self, string_image):
+        dt_now = datetime.now()
+        dt_formatted = dt_now.strftime("%Y-%m-%d %H-%M-%S-%f")
+        data = {
+            "username": self.username,
+            "send_at": dt_formatted,
+            "blb_image": string_image
+        }
+        self.sio.emit('capture', json.dumps(data))
+        self.sio.wait()
+
